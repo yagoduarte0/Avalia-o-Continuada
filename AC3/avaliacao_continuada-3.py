b69b5b79fbc9d6af9e15aba53382f7593ff5b3ae
Yago Duarte de Andrade <160678426+yagoduarte0@users.noreply.github.com>
2024-03-13 20:49:13 -0300

    Add files via upload
    Incluindo avaliação continuada 3.

---
+++ AC3/avaliacao_continuada-3.py
@@ -0,0 +1,24 @@
+"""
+Programacao Estruturada
+2024.1
+11/03/2024
+Yago Duarte de Andrade
+
+AC2 (Exerc: 1) - 
+AC2 (Exerc: 2) - 
+"""
+
+def determina_tipo_triangulo():
+    a = float(input("Informe o primeiro lado: "))
+    b = float(input("Informe o segundo lado: "))
+    c = float(input("Informe o terceiro lado: "))
+    if (a + b < c) or (a + c < b) or (b + c < a):
+        print("Não é um triangulo!")
+    elif (a == b == c):
+        print("Equilátero")
+    elif (a != b != c != a):
+        print("Escaleno")
+    else:
+        print("Isóceles")
+
+determina_tipo_triangulo()
